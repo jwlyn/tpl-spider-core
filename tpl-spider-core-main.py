3b63f42a4462436f7f9bb786a0e89e8d1a2c0d90
Jon <xuchaoo@gmail.com>
2019-03-18 18:30:23 +0800

    cm

--- tpl-spider-core-main.py
+++ tpl-spider-core-main.py
@@ -16,162 +16,153 @@ import random
 
 from utils import send_email
 
-"""
 
-"""
+class SpiderTask(object):
+    def __init__(self):
+        self.db = psycopg2.connect(database=dbconfig.db_name, user=dbconfig.db_user, password=dbconfig.db_psw,
+                          host=dbconfig.db_url, port=dbconfig.db_port)
 
-db = psycopg2.connect(database=dbconfig.db_name, user=dbconfig.db_user, password=dbconfig.db_psw,
-                      host=dbconfig.db_url, port=dbconfig.db_port)
+    def __get_task_by_sql(self, sql):
+        db_trans = psycopg2.connect(database=dbconfig.db_name, user=dbconfig.db_user, password=dbconfig.db_psw,
+                                    host=dbconfig.db_url, port=dbconfig.db_port)
+        db_trans.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_REPEATABLE_READ)
+        cursor = db_trans.cursor()
 
-
-def __get_task_by_sql(sql):
-    db_trans = psycopg2.connect(database=dbconfig.db_name, user=dbconfig.db_user, password=dbconfig.db_psw,
-                                host=dbconfig.db_url, port=dbconfig.db_port)
-    db_trans.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_REPEATABLE_READ)
-    cursor = db_trans.cursor()
-    row=None
-    try:
-        cursor.execute(sql)
-        if cursor.rowcount>0:
-            row = cursor.fetchone()
-            db_trans.commit()
-        else:
-            return None
-    except TransactionRollbackError as multip_update_exp:
-        logger.info(multip_update_exp)
-        db_trans.rollback()
-        return None
-    except (DatabaseError, ProgrammingError, OperationalError) as dbe:
-        logger.info(dbe)
-        db_trans.rollback()
-        return None
-    finally:
-        db_trans.close()
-
-    if row is None:
-        return None
-
-    r = row
-    cursor.close()
-    task = {
-        'id': r[0],
-        'seeds': json.loads(r[1]),
-        'ip': r[2],
-        'user_id_str': r[3],
-        'user_agent': r[4],
-        'status': r[5],
-        'is_grab_out_link': r[6],
-        'gmt_modified': r[7],
-        'gmt_created': r[8],
-        'file_id': r[9],
-    }
-
-    return task
-
-
-def __get_timeout_task():
-    sql = """
-        -- start transaction isolation level repeatable read;
-        update spider_task set gmt_modified = NOW() where id in (
-            select id
-            from spider_task
-            where status ='P' AND gmt_modified + '10 minutes'::INTERVAL < NOW()
-            order by gmt_created DESC 
-            limit 1
-        )
-        returning id, seeds, ip, user_id_str, user_agent, status, is_grab_out_link, gmt_modified, gmt_created,file_id;
-        -- commit;
-    """
-    return __get_task_by_sql(sql)
-
-
-def __get_a_task():
-    sql = """
-        -- start transaction isolation level repeatable read;
-        update spider_task set status = 'P', gmt_modified=NOW() where id in (
-            select id
-            from spider_task
-            where status ='I'
-            order by gmt_created DESC 
-            limit 1
-        )
-        returning id, seeds, ip, user_id_str, user_agent, status, is_grab_out_link, gmt_modified, gmt_created, file_id;
-        -- commit;
-    """
-    return __get_task_by_sql(sql)
-
-
-def __update_task_finished(task_id, zip_path, status='C'):
-    sql = f"""
-        update spider_task set status = '{status}', result='{zip_path}' where id = '{task_id}';
-    """
-    cursor = db.cursor()
-    logger.info("begin execute sql %s", sql)
-    cursor.execute(sql)
-    logger.info("after execute sql %s", sql)
-    cursor.close()
-    logger.info("close execute sql %s", sql)
-    db.commit()
-    logger.info("commit execute sql %s", sql)
-
-
-def __get_user_agent(key):
-    ua_list = config.ua_list.get(key)
-    if ua_list is None:
-        ua = config.default_ua
-    else:
-        return random.choice(ua_list)
-
-    return ua
-
-
-async def __do_process(base_craw_file_dir):
-
-    while True:
-        task = __get_timeout_task()  # 优先处理超时的任务
-
-        if task is not None:
-            logger.info("获得一个超时任务 %s", task['id'])
-        else:
-            task = __get_a_task()
-            if not task:
-                logger.info("no task, wait")
-                await asyncio.sleep(config.wait_db_task_interval_s)
-                continue
+        try:
+            cursor.execute(sql)
+            if cursor.rowcount>0:
+                row = cursor.fetchone()
+                db_trans.commit()
             else:
-                logger.info("获得一个正常任务 %s", task['id'])
+                return None
+        except TransactionRollbackError as multip_update_exp:
+            logger.info(multip_update_exp)
+            db_trans.rollback()
+            return None
+        except (DatabaseError, ProgrammingError, OperationalError) as dbe:
+            logger.info(dbe)
+            db_trans.rollback()
+            return None
+        finally:
+            db_trans.close()
 
-        seeds = task['seeds']
-        is_grab_out_site_link = task['is_grab_out_link'] #是否抓取外部站点资源
-        user_agent = __get_user_agent(task['user_agent'])
-        spider = TemplateCrawler(seeds, save_base_dir=f"{base_craw_file_dir}/",
-                                 header={'User-Agent': user_agent},
-                                 grab_out_site_link=is_grab_out_site_link)
-        template_zip_file = await spider.template_crawl()
-        logger.info("begin update task finished")
-        __update_task_finished(task['id'], template_zip_file)
-        send_email("web template download link", f"http://template-spider.com/get-web-template/{task['file_id']}", task['user_id_str'])
-        logger.info("send email to %s, link: %s", task['user_id_str'], task['file_id'])
+        if row is None:
+            return None
 
+        r = row
+        cursor.close()
+        task = {
+            'id': r[0],
+            'seeds': json.loads(r[1]),
+            'ip': r[2],
+            'user_id_str': r[3],
+            'user_agent': r[4],
+            'status': r[5],
+            'is_grab_out_link': r[6],
+            'gmt_modified': r[7],
+            'gmt_created': r[8],
+            'file_id': r[9],
+        }
 
-def __process_thread(base_craw_file_dir):
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.gather(
-        __do_process(base_craw_file_dir),
-    ))
-    loop.close()
+        return task
 
+    def __get_timeout_task(self):
+        sql = """
+            -- start transaction isolation level repeatable read;
+            update spider_task set gmt_modified = NOW() where id in (
+                select id
+                from spider_task
+                where status ='P' AND gmt_modified + '10 minutes'::INTERVAL < NOW()
+                order by gmt_created DESC 
+                limit 1
+            )
+            returning id, seeds, ip, user_id_str, user_agent, status, is_grab_out_link, gmt_modified, gmt_created,file_id;
+            -- commit;
+        """
+        return self.__get_task_by_sql(sql)
 
-def __create_process(base_craw_file_dir):
-    process_arr = []
-    process_cnt = config.max_spider_process
+    def __get_a_task(self):
+        sql = """
+            -- start transaction isolation level repeatable read;
+            update spider_task set status = 'P', gmt_modified=NOW() where id in (
+                select id
+                from spider_task
+                where status ='I'
+                order by gmt_created DESC 
+                limit 1
+            )
+            returning id, seeds, ip, user_id_str, user_agent, status, is_grab_out_link, gmt_modified, gmt_created, file_id;
+            -- commit;
+        """
+        return self.__get_task_by_sql(sql)
 
-    for i in range(0, process_cnt):
-        p = Process(target=__process_thread, args=(base_craw_file_dir,))
-        process_arr.append(p)
-        p.start()
+    def __update_task_finished(self, task_id, zip_path, status='C'):
+        sql = f"""
+            update spider_task set status = '{status}', result='{zip_path}' where id = '{task_id}';
+        """
+        cursor = self.db.cursor()
+        logger.info("begin execute sql %s", sql)
+        cursor.execute(sql)
+        logger.info("after execute sql %s", sql)
+        cursor.close()
+        logger.info("close execute sql %s", sql)
+        self.db.commit()
+        logger.info("commit execute sql %s", sql)
 
-    return process_arr
+    def __get_user_agent(self, key):
+        ua_list = config.ua_list.get(key)
+        if ua_list is None:
+            ua = config.default_ua
+        else:
+            return random.choice(ua_list)
+
+        return ua
+
+    async def __do_process(self, base_craw_file_dir):
+
+        while True:
+            task = self.__get_timeout_task()  # 优先处理超时的任务
+
+            if task is not None:
+                logger.info("获得一个超时任务 %s", task['id'])
+            else:
+                task = self.__get_a_task()
+                if not task:
+                    logger.info("no task, wait")
+                    await asyncio.sleep(config.wait_db_task_interval_s)
+                    continue
+                else:
+                    logger.info("获得一个正常任务 %s", task['id'])
+
+            seeds = task['seeds']
+            is_grab_out_site_link = task['is_grab_out_link'] #是否抓取外部站点资源
+            user_agent = self.__get_user_agent(task['user_agent'])
+            spider = TemplateCrawler(seeds, save_base_dir=f"{base_craw_file_dir}/",
+                                     header={'User-Agent': user_agent},
+                                     grab_out_site_link=is_grab_out_site_link)
+            template_zip_file = await spider.template_crawl()
+            logger.info("begin update task finished")
+            self.__update_task_finished(task['id'], template_zip_file)
+            send_email("web template download link", f"http://template-spider.com/get-web-template/{task['file_id']}", task['user_id_str'])
+            logger.info("send email to %s, link: %s", task['user_id_str'], task['file_id'])
+
+    def __process_thread(self, base_craw_file_dir):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(asyncio.gather(
+            self.__do_process(base_craw_file_dir),
+        ))
+        loop.close()
+
+    def create_process(self, base_craw_file_dir):
+        process_arr = []
+        process_cnt = config.max_spider_process
+
+        for i in range(0, process_cnt):
+            p = Process(target=self.__process_thread, args=(base_craw_file_dir,))
+            process_arr.append(p)
+            p.start()
+
+        return process_arr
 
 
 if __name__ == "__main__":
@@ -180,7 +171,9 @@ if __name__ == "__main__":
     logger.info("基本目录是%s", base_craw_file_dir)
     if not base_craw_file_dir:
         logger.error("没有指明模版压缩文件的目录")
-    process = __create_process(base_craw_file_dir)
+
+    task = SpiderTask()
+    process = task.create_process(base_craw_file_dir)
     while True:
         time.sleep(100)
     db.close()
